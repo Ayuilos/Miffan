38b450962624a3099f5421747dddb37bdb9398a4
Ayuilos <ekklovepeace@gmail.com>
2026-09-14 10:52:35 +0800

    chore: prepare 3.3.0 stable release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "me.ayuilos.miffan.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 178014
-        versionName = "3.3.0-beta.2"
+        versionCode = 178015
+        versionName = "3.3.0"
 
         buildConfigField("boolean", "FIREBASE_ENABLED", hasGoogleServicesConfig.toString())
 
