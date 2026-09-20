d2feb6c3d11dc8b213973069fbf0948a38bebf15
Ayuilos <ekklovepeace@gmail.com>
2026-09-20 21:21:02 +0800

    chore: prepare Miffan 3.4.2 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "me.ayuilos.miffan.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 178019
-        versionName = "3.4.1"
+        versionCode = 178020
+        versionName = "3.4.2"
 
         buildConfigField("boolean", "FIREBASE_ENABLED", hasGoogleServicesConfig.toString())
 
